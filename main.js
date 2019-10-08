9d5c190ad12301f9a5ffd69516f2ed0909e011c5
phenix1229 <paul.garay@codeimmersives.com>
2019-10-08 15:51:26 -0400

    added content

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const div1 = document.querySelector('#div1');
 const div2 = document.querySelector('#div2');
 const div3 = document.querySelector('#div3');
 const div4 = document.querySelector('#div4');
+const div2P = div2.querySelector('p');
 
-body.style.backgroundImage = 'url(https://media0.giphy.com/media/6Nv7BdrUl6Psc/giphy.gif)'
-body.style.backgroundRepeat = 'repeat-y'
+body.style.backgroundImage = 'url(https://media0.giphy.com/media/6Nv7BdrUl6Psc/giphy.gif)';
+body.style.backgroundRepeat = 'repeat-y';
 
 tText1.style.fontSize = '500px';
 tText1.style.color = 'rgba(250,150,50,0.5)';
@@ -15,4 +16,15 @@ tText1.style.color = 'rgba(250,150,50,0.5)';
 tText2.style.fontSize = '50px';
 tText2.style.color = 'rgba(50,150,250,.75';
 
-div1.style.background = 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(121,119,9,0.8617647742690826) 37%, rgba(0,212,255,1) 100%)'
+div1.style.background = 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(121,119,9,0.8617647742690826) 37%, rgba(0,212,255,1) 100%)';
+
+div2.style.margin = '50px 100px 150px 200px';
+div2.style.background = 'lightBlue';
+div2.style.padding = '20px';
+
+div2P.style.color = 'yellow';
+
+div3.style.margin = '0px';
+div3.style.padding = '100px';
+div3.style.backgroundImage = 'url(https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiCr6L9to3lAhWypFkKHQQOAy4QjRx6BAgBEAQ&url=https%3A%2F%2Fmedium.com%2Fbigger-picture%2Foompa-loompas-and-the-sad-state-of-the-music-industry-1206ccf4e3f6&psig=AOvVaw2gWtbUKmyyd04uLf3e1ZSb&ust=1570650438529714)';
+div3.style.backgroundRepeat ='repeat-x'
