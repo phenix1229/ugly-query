61226dfcbd265c116d97cc7a3891f138d4d1016d
phenix1229 <paul.garay@codeimmersives.com>
2019-10-08 15:36:15 -0400

    it's a start

---
+++ main.js
@@ -0,0 +1,18 @@
+const body = document.querySelector('body');
+const tText1 = document.querySelector('#t-text1');
+const tText2 = document.querySelector('#t-text2');
+const div1 = document.querySelector('#div1');
+const div2 = document.querySelector('#div2');
+const div3 = document.querySelector('#div3');
+const div4 = document.querySelector('#div4');
+
+body.style.backgroundImage = 'url(https://media0.giphy.com/media/6Nv7BdrUl6Psc/giphy.gif)'
+body.style.backgroundRepeat = 'repeat-y'
+
+tText1.style.fontSize = '500px';
+tText1.style.color = 'rgba(250,150,50,0.5)';
+
+tText2.style.fontSize = '50px';
+tText2.style.color = 'rgba(50,150,250,.75';
+
+div1.style.background = 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(121,119,9,0.8617647742690826) 37%, rgba(0,212,255,1) 100%)'
