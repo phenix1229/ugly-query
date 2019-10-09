123ced8717345854dab7f47b829107dabcaf63d6
phenix1229 <paul.garay@codeimmersives.com>
2019-10-09 11:31:53 -0400

    done

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const div3Ul = div3.querySelector('#ul1');
 const div3Li = div3.querySelector('.li-d3');
 const form = document.querySelector('form');
 const textArea = document.querySelector('textarea');
+const mainDiv = document.querySelector('#mainDiv');
 
 body.style.backgroundImage = 'url(https://media0.giphy.com/media/6Nv7BdrUl6Psc/giphy.gif)';
 body.style.backgroundRepeat = 'repeat-y';
@@ -21,6 +22,7 @@ tText2.style.fontSize = '50px';
 tText2.style.color = 'rgba(50,150,250,.75';
 
 div1.style.background = 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(121,119,9,0.8617647742690826) 37%, rgba(0,212,255,1) 100%)';
+div1.style.position = 'absolute'
 
 div2.style.margin = '50px 100px 150px 200px';
 div2.style.background = 'lightBlue';
